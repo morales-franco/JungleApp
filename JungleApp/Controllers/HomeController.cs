e35c71dcee85b1f91c3139b2859ddb2df676f573
Morales Franco <franco.e.morales@gmail.com>
2019-01-27 19:46:07 -0300

    Upload project. Topic: readonly & const keyword

---
+++ JungleApp/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using JungleApp.Models;
+
+namespace JungleApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /*
+         * Action Method
+         */
+        public IActionResult ReadonlyKeyword()
+        {
+            var dog = new Dog(DateTime.Now, "Pitbull", "Rambito");
+
+            dog.Name = "Diana";
+            dog.Breed = "Bulldog";
+
+            /*
+             * Readonly variables! They can only be asssigned in a constructor or declaration in the class
+             */
+            //dog.Birth = DateTime.Now;
+            //dog.Color = "Green";
+
+            /*
+             * Constant !
+             * DefaultMood is a constant, its value can't be changed in execution time. 
+             * This variable can only be assigned in its declaration.
+             */
+            //Dog.DefaultMood = "Bored";
+
+            return View(dog);
+        }
+
+
+    }
+}
---
+++ JungleApp/Models/Dog.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JungleApp.Models
+{
+    public class Dog
+    {
+        public const string DefaultMood = "Happy"; //const: assignment only in declaration
+
+        public readonly DateTime Birth;
+        public readonly string Color = "Gray"; //assignment in declaration
+
+        public string Breed { get;  set; }
+        public string Name { get;  set; }
+
+        public string Mood { get; private set; } 
+        
+        public Dog(DateTime birth, string breed, string name)
+        {
+            if (string.IsNullOrEmpty(breed) || string.IsNullOrEmpty(name))
+                throw new ArgumentNullException();
+
+            Birth = birth; //assignment in constructor
+            Breed = breed;
+            Name = name;
+            Mood = DefaultMood;//assign default value
+        }
+
+        //It's not possible! Assigment is posible only in Declaration & Constructor
+        //public void ChangeColor(string newColor) {
+        //    Color = newColor;
+        //}
+
+    }
+}
